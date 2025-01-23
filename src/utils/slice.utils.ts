d1e820444c2f1f53334fae89f3984c7586644a64
luizfm <luiz.ado@hotmail.com>
2025-01-22 22:58:22 -0300

    chore: rename .env file

--- src/utils/slice.utils.ts
+++ src/utils/slice.utils.ts
@@ -1,16 +1,15 @@
 interface GetSliceData<T> {
-    slice: T;
-    mockedData: any;
+  slice: T;
+  mockedData: any;
 }
 
 export const sliceUtils = {
-    getSliceData<T>({ mockedData, slice }: GetSliceData<T>): T {
-        const typedSlice = slice as any
-        const slicePrimaryValues = Object.values(typedSlice.primary as any)
-        const hasAnyPrimaryValues = slicePrimaryValues.some((value) => !!value)
-        const hasAnyValue = hasAnyPrimaryValues || typedSlice.items.length > 0
+  getSliceData<T>({ mockedData, slice }: GetSliceData<T>): T {
+    const typedSlice = slice as any;
+    const slicePrimaryValues = Object.values(typedSlice.primary as any);
+    const hasAnyPrimaryValues = slicePrimaryValues.some((value) => !!value);
+    const hasAnyValue = hasAnyPrimaryValues || typedSlice.items.length > 0;
 
-        console.log('aqui', slice, mockedData)
-        return hasAnyValue ? slice : mockedData[0]
-    }
-}
+    return hasAnyValue ? slice : mockedData[0];
+  },
+};
